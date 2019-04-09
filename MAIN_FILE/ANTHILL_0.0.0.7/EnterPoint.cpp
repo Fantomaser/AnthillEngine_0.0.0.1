c0f78770c6a4dcaac7ce52002fdb6f4724ddca70
Fantomaser <Evgenuy1605@yandex.ru>
2019-04-09 22:57:25 +0300

    linux window

--- MAIN_FILE/ANTHILL_0.0.0.7/EnterPoint.cpp
+++ MAIN_FILE/ANTHILL_0.0.0.7/EnterPoint.cpp
@@ -1,8 +1,40 @@
+#include <stdio.h>
+#include <X11/Xlib.h>
 
-#include <iostream>
+int main()
+ {
+  Display *ourDisplay;
 
-int main ()
-{
-    std::cout<< "Hellow world from linux" << std::endl;
-    return 0;
-}
+  ourDisplay=XOpenDisplay(NULL);
+  if (ourDisplay==NULL)
+    {
+      printf("Не удалось установить соединение с графическим терминалом.\n");
+      return 1;
+    };
+
+  /* Получим предварительные сведения */
+  int ourScreen=DefaultScreen(ourDisplay);          // Экран по-умолчанию
+int rootWindow=RootWindow(ourDisplay, ourScreen); // Корневое окно
+ int bgcolor=WhitePixel(ourDisplay, ourScreen);    // Белый цвет экрана
+
+  /* Открываем окно */
+int  myWindow=XCreateSimpleWindow(ourDisplay,rootWindow,100, 100, 320, 200,
+    0, 0, bgcolor);
+
+  /* Делаем окно видимым */
+  XMapWindow(ourDisplay, myWindow);
+
+  /* Все сформированные команды принудительно сбрасываем на сервер */
+  XFlush(ourDisplay);
+
+  getchar();
+  /* Спим */
+
+  /* Уничтожаем окно */
+  XDestroyWindow(ourDisplay, myWindow);
+
+  /* Закрываем соединение с сервером */
+  XCloseDisplay(ourDisplay);
+
+  return 0;
+ };
